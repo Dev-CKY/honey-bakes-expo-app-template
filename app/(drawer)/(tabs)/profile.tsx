fec240ad714361285218347bd96052cf6de13f5b
kodevendor <developer@kodevendor.com>
2026-05-30 14:06:42 +0530

    update my-profile screen add account swicth options

--- app/(drawer)/(tabs)/profile.tsx
+++ app/(drawer)/(tabs)/profile.tsx
@@ -1,5 +1,7 @@
 import arrowLeft from "@/src/assets/icons/svg/arrowLeft";
+import logout2 from "@/src/assets/icons/svg/logout2";
 import pencil from "@/src/assets/icons/svg/pencil";
+import plus from "@/src/assets/icons/svg/plus";
 import HeadingTitle from "@/src/components/custom/HeadingTitle";
 import IconButtonWrapper from "@/src/components/custom/IconButtonWrapper";
 import Routes from "@/src/components/custom/Routes";
@@ -57,6 +59,45 @@ const Profile = () => {
 
         {/* Routes */}
         <Routes data={PROFILE_ROUTES} />
+
+        {/* Others section */}
+        <Text className="text-[20px] p-[20px] text-[#1F1500] font-[kalnia-medium] mt-[5px]">
+          Others
+        </Text>
+
+        <View className="flex-row items-center justify-between px-[20px]">
+          {/* Account and add account icon */}
+          <View className="flex-row items-center gap-[20px]">
+            <View className="items-center">
+              <Image
+                source={require("@/src/assets/images/custom/profile.jpg")}
+                className="h-[54px] w-[54px] rounded-full"
+              />
+              <Text className="text-[14px]  text-[#1F1500] font-[poppins-regular] mt-[2px]">
+                You
+              </Text>
+            </View>
+
+            <Pressable className="items-center">
+              <View className="h-[54px] w-[54px] rounded-full bg-[#F6F0D4] items-center justify-center">
+                <SvgXml xml={plus} />
+              </View>
+              <Text className="text-[14px]  text-[#1F1500] font-[poppins-regular] mt-[2px]">
+                Add
+              </Text>
+            </Pressable>
+          </View>
+
+          {/* Logout */}
+          <Pressable className="items-center">
+            <View className="h-[54px] w-[54px] rounded-full bg-[#F7BC5D] border border-[1.5px] border-[#1F1500] items-center justify-center">
+              <SvgXml xml={logout2} />
+            </View>
+            <Text className="text-[14px]  text-[#1F1500] font-[poppins-regular] mt-[2px]">
+              Logout
+            </Text>
+          </Pressable>
+        </View>
       </>
     </ScrollView>
   );
